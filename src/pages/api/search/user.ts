26a94b0085e9a90c2259fa866b5483ad6ae3fc7a
saheenshoukath <saheenshoukath@gmail.com>
2022-04-24 22:22:13 +0530

    :sparkles: add search user api handler

---
+++ src/pages/api/search/user.ts
@@ -0,0 +1,41 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import { prisma } from '@/lib/prisma'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { query } = req.query as {
+    query: string
+  }
+
+  if (!query) {
+    return res.status(400).json({ error: 'No query' })
+  }
+
+  try {
+    const users = await prisma.user.findMany({
+      where: {
+        OR: [
+          {
+            name: {
+              contains: query,
+              mode: 'insensitive',
+            },
+          },
+          {
+            username: {
+              contains: query,
+              mode: 'insensitive',
+            },
+          },
+        ],
+      },
+    })
+    res.status(200).json({ users })
+  } catch (error) {
+    const err = error as Error
+    res.status(500).json({ error: err.message })
+  }
+}
