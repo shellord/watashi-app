62e5a67d351cd294c319f760f7fedb28cf0b08ba
saheenshoukath <saheenshoukath@gmail.com>
2022-05-01 18:09:11 +0530

    add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,15 @@ export default function Document() {
           href='https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap'
           rel='stylesheet'
         />
+        <meta
+          name='viewport'
+          content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no'
+        />
+        <meta name='description' content='Description' />
+        <meta name='keywords' content='Keywords' />
+        <link rel='manifest' href='/manifest.webmanifest' />
+        <link rel='apple-touch-icon' href='images/logo.png '></link>
+        <meta name='theme-color' content='#ffffff' />
       </Head>
       <body>
         <Main />
