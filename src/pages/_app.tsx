79daf82c0b659035e33ddbcd9056bf2f8d45e68f
saheenshoukath <saheenshoukath@gmail.com>
2022-05-08 21:51:25 +0530

    :lipstick: remove top spinner

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,6 +13,8 @@ import MainLayout from '@/components/layouts/MainLayout'
 
 const queryClient = new QueryClient()
 
+nProgress.configure({ showSpinner: false })
+
 Router.events.on('routeChangeStart', nProgress.start)
 Router.events.on('routeChangeError', nProgress.done)
 Router.events.on('routeChangeComplete', nProgress.done)
