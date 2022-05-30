058e83f72219761892c52ce27e32c2750f29945e
saheenshoukath <saheenshoukath@gmail.com>
2022-05-30 22:35:29 +0530

    add bottom sheet with gesture

--- src/components/ui/BottomSheet.tsx
+++ src/components/ui/BottomSheet.tsx
@@ -1,4 +1,7 @@
+import { useDrag } from '@use-gesture/react'
 import clsx from 'clsx'
+import { useEffect } from 'react'
+import { animated, config, useSpring } from 'react-spring'
 
 type Props = {
   show: boolean
@@ -8,12 +11,64 @@ type Props = {
 }
 
 export const BottomSheet = ({ show, className, setShow, children }: Props) => {
+  const height = window.innerHeight / 2
+
+  const [{ y }, api] = useSpring(() => ({ y: height }))
+
+  const bind = useDrag(
+    ({
+      last,
+      movement: [_mx, my],
+      velocity: [_vx, vy],
+      direction: [_dx, dy],
+      cancel,
+      canceled,
+    }) => {
+      if (my < -100) {
+        cancel()
+      }
+      if (last) {
+        my > height * 0.5 || (vy > 0.5 && dy > 0)
+          ? close(vy)
+          : open({ canceled })
+      } else api.start({ y: my })
+    },
+    {
+      from: () => [0, y.get()],
+      filterTaps: true,
+      bounds: { top: 0 },
+      rubberband: true,
+    }
+  )
+
+  const open = ({ canceled }: { canceled: boolean }) => {
+    api.start({
+      y: 0,
+      immediate: false,
+      config: canceled ? config.wobbly : config.stiff,
+    })
+  }
+
+  const close = (velocity = 0) => {
+    api.start({ y: height, config: { ...config.stiff, velocity } })
+    setShow(false)
+  }
+
+  useEffect(() => {
+    if (show) {
+      open({ canceled: false })
+    } else {
+      close()
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [show])
+
   return (
     <>
       {/* Backdrop */}
       <div
         className={clsx(
-          'bg-black bg-opacity-60 h-screen flex items-end fixed inset-0 z-40 transition-opacity duration-500',
+          'bg-black/60 h-screen flex items-end fixed inset-0 z-40 transition-opacity duration-500',
           {
             'opacity-0 pointer-events-none': !show,
             'opacity-100': show,
@@ -23,22 +78,16 @@ export const BottomSheet = ({ show, className, setShow, children }: Props) => {
         key='bottom-sheet'
       />
       {/* Bottom Sheet */}
-      <div className='w-'>
-        <div
-          className={clsx(
-            'h-1/2 fixed bottom-0 right-0 left-0 w-full transition-all duration-500 z-50 shadow-2xl rounded-t-xl',
-            {
-              'translate-y-full': !show,
-              'translate-y-0': show,
-            },
-
-            className
-          )}
-          onClick={(event) => event.stopPropagation()}
-        >
-          {children}
-        </div>
-      </div>
+      <animated.div
+        {...bind()}
+        style={{ y, touchAction: 'none', bottom: `calc(-100vh + ${height}px)` }}
+        className={clsx(
+          'h-screen fixed bottom-0 right-0 left-0 w-full z-50 shadow-2xl rounded-t-xl',
+          className
+        )}
+      >
+        {children}
+      </animated.div>
     </>
   )
 }
