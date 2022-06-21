f7fb09b7644bd245807a13cda589a0e1a6236428
saheenshoukath <saheenshoukath@gmail.com>
2022-06-21 21:55:07 +0530

    remove meta seo

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,8 +8,6 @@ export default function Document() {
           href='https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap'
           rel='stylesheet'
         />
-        <meta name='description' content='Description' />
-        <meta name='keywords' content='Keywords' />
         <link rel='apple-touch-icon' href='images/logo.png ' />
         <meta name='theme-color' content='#ffffff' />
       </Head>
